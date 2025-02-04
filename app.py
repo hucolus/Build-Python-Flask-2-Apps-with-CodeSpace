962fe8c6c7bc122429f61fe7b50d97460ff0f1b1
hucolus <hucolus@gmail.com>
2025-02-04 02:55:37 +0000

    finish lab02

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/index')
+def index():
+    user={'username': 'John'},
+    posts=[
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+if __name__ =='__main__':
+    app.run()
